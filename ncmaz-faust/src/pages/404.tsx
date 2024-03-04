f28ee046cb2727517d36ba29a3af3a574a28ba24
tolgaaslan <tolgaarslan@mail.com>
2024-03-04 10:02:41 +0300

    first setup

---
+++ ncmaz-faust/src/pages/404.tsx
@@ -0,0 +1,16 @@
+import Page404Content from "@/container/404Content";
+
+export default function Page404() {
+  return (
+    <>
+      <Page404Content />
+    </>
+  );
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+    revalidate: 900,
+  };
+};
