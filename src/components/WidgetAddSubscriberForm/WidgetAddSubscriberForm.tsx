b41e2dd51ef7ffe6e037565b6b423bca641570bb
tolgaaslan <tolgaarslan@mail.com>
2024-03-27 17:37:57 +0300

    old backup widgedaddsubsriberform

--- src/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm.tsx
+++ src/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm.tsx
@@ -14,26 +14,42 @@ const WidgetAddSubscriberForm: FC<Props> = ({
   if (NC_SITE_SETTINGS.subcription_widget.enable === false) {
     return null;
   }
-
   return (
     <div className={`nc-WidgetAddSubscriberForm overflow-hidden ${className}`}>
-      
       <WidgetHeading1
         title={NC_SITE_SETTINGS.subcription_widget.title}
-        icon={<div className="h-6 w-6 flex-none" >👑</div>}
+        icon={<EnvelopeIcon className="h-6 w-6 flex-none" />}
       />
-      
       <div className="p-4 xl:p-5">
         <span className="mt-2 text-sm leading-6 text-neutral-600 dark:text-neutral-400">
           {NC_SITE_SETTINGS.subcription_widget.description}
         </span>
         <div className="mt-4">
-        <a href="https://auraajans.net/hakkimizda" target="_self" className="whitespace-nowrap font-semibold">Hakkımızda  →</a>
-        <a href="https://auraajans.net/iletisim" target="_self" className="whitespace-nowrap font-semibold float-right">İletişim  →</a>
+          <AddSubscriberForm />
         </div>
       </div>
     </div>
   );
+
+  // return (
+  //   <div className={`nc-WidgetAddSubscriberForm overflow-hidden ${className}`}>
+      
+  //     <WidgetHeading1
+  //       title={NC_SITE_SETTINGS.subcription_widget.title}
+  //       icon={<div className="h-6 w-6 flex-none" >👑</div>}
+  //     />
+      
+  //     <div className="p-4 xl:p-5">
+  //       <span className="mt-2 text-sm leading-6 text-neutral-600 dark:text-neutral-400">
+  //         {NC_SITE_SETTINGS.subcription_widget.description}
+  //       </span>
+  //       <div className="mt-4">
+  //       <a href="https://auraajans.net/hakkimizda" target="_self" className="whitespace-nowrap font-semibold">Hakkımızda  →</a>
+  //       <a href="https://auraajans.net/iletisim" target="_self" className="whitespace-nowrap font-semibold float-right">İletişim  →</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default WidgetAddSubscriberForm;
